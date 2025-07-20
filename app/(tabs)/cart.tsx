5d46b4fa78a204700d8cdafb2248150fdc7a9687
Qazi Areeb <areeb.999@gmail.com>
2025-07-20 13:25:47 +0530

    Implement cart funcitonality in app

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -1,11 +1,68 @@
-import { View, Text } from 'react-native'
+import CartItem from '@/components/CartItem'
+import Custombutton from '@/components/CustomButton'
+import CustomHeader from '@/components/Customheader'
+import { useCartStore } from '@/store/cart.store'
+import { PaymentInfoStripeProps } from '@/type'
+import cn from 'clsx'
 import React from 'react'
+import { FlatList, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const PaymentInfoStripe = ({ label,  value,  labelStyle,  valueStyle, }: PaymentInfoStripeProps) => (
+    <View className="flex-between flex-row my-1">
+        <Text className={cn("paragraph-medium text-gray-200", labelStyle)}>
+            {label}
+        </Text>
+        <Text className={cn("paragraph-bold text-dark-100", valueStyle)}>
+            {value}
+        </Text>
+    </View>
+);
 
 const cart = () => {
+  const {items , getTotalItems , getTotalPrice} = useCartStore()
+  const totalItems = getTotalItems();
+  const totalPrice = getTotalPrice();
+
   return (
-    <View>
-      <Text>cart</Text>
-    </View>
+    <SafeAreaView className='bg-white h-full'>
+       <FlatList
+       data={items} 
+       renderItem={({item})=><CartItem item={item}/>}
+       keyExtractor={(item)=>item.id}
+      contentContainerClassName='pb-28 px-5 pt-5'
+      ListHeaderComponent={()=><CustomHeader title='Your cart'/>}
+      ListEmptyComponent={()=><Text>Cart Empty </Text>}
+      ListFooterComponent={()=> totalItems>0 &&(
+        <View className='gap-5'>
+          <View className='mt-6 border-gray-200 p-5 rounded-2xl'>
+            <Text className='h3-bold text-dark-100 mb-5'>
+              payment Summery
+            </Text>
+            <PaymentInfoStripe
+            label={`Total Items (${totalItems})`}
+            value={`${totalPrice.toFixed(2)}`}            
+            />
+            <PaymentInfoStripe
+            label={`Delivery fee`}
+            value={`$5.00`}            
+            />
+            <PaymentInfoStripe
+            label={`Discount`}
+            value={`-0.50`} 
+            valueStyle='!text-success'           
+            />
+            <View className='border-t border-gray-300 my-2'/>
+             <PaymentInfoStripe
+            label={`Total`}
+            value={`${(totalPrice+5-0.5).toFixed(2)}`}            
+            />
+          </View>
+          <Custombutton title='Order now'/>
+        </View>
+      )}
+/>
+    </SafeAreaView>
   )
 }
 
--- components/CartButton.tsx
+++ components/CartButton.tsx
@@ -1,11 +1,14 @@
 import { images } from '@/constants'
+import { useCartStore } from '@/store/cart.store'
+import { router } from 'expo-router'
 import React from 'react'
 import { Image, Text, TouchableOpacity, View } from 'react-native'
 
 const CartButton = () => {
-    const totalItems = 10
+  const {getTotalItems} = useCartStore();
+    const totalItems = getTotalItems()
   return (
-   <TouchableOpacity className="flex items-center justify-center size-10 rounded-full bg-dark-100" onPress={()=>{}}>
+   <TouchableOpacity className="flex items-center justify-center size-10 rounded-full bg-dark-100" onPress={()=>{router.push('/(tabs)/cart')}}>
     <Image source={images.bag} className='size-5' resizeMode='contain'/>
     {totalItems >0 &&(
         <View className='cart-badge'>
